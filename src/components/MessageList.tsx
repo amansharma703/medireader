1b2909dd96b18e9be1186dac647c803ee845f833
amansharma703 <aman703jk@gmail.com>
2024-10-07 16:17:48 +0530

    ui fixes

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -34,7 +34,7 @@ const MessageList = ({ messages, isLoading }: Props) => {
           >
             <div
               className={cn(
-                "rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10",
+                "rounded-lg px-3 py-1 shadow-md ring-1 ring-gray-900/10",
                 {
                   "bg-blue-600 text-white": message.role === "user",
                 }
